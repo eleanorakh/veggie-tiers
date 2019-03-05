b1b654939311790c39aa38f6357054625b660900
Eleanor Kiefel Haggerty <eleanorakh@gmail.com>
2019-03-05 21:38:57 +1100

    create app  component

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import 'bulma/bulma';
+
+class App extends Component {
+  render() {
+    return(
+      ...
+    )
+  }
+}
